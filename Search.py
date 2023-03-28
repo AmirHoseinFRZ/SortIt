c6dd095f116ddd1e66cf8712cd0df030a4d0028c
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2023-03-28 14:25:48 +0330

    first commit

---
+++ Search.py
@@ -0,0 +1,23 @@
+from Solution import Solution
+from Problem import Problem
+from datetime import datetime
+
+
+class Search:
+    @staticmethod
+    def bfs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+        # solution is find return None else return the solution
+        print(prb.initState)
+        start_time = datetime.now()
+        queue = []
+        state = prb.initState
+        queue.append(state)
+        while len(queue) > 0:
+            state = queue.pop(0)
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if prb.is_goal(c):
+                    print(c)
+                    return Solution(c, prb, start_time)
+                queue.append(c)
+        return None
