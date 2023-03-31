b43bc7be1980695fc417251291a623e1c346c325
Amirreza <amirrezasalarimanesh1302@gmail.com>
2023-03-31 20:56:27 +0330

    add back_get_cost_from_change and back_successor for BDS

--- Problem.py
+++ Problem.py
@@ -33,6 +33,19 @@ class Problem:
                     child.append(State(copy.deepcopy(s.pipes), state, self.get_cost_from_change(state, i), (i, j)))
         return child
 
+    # this method for every state gives every possible states form this self and return it
+    def back_successor(self, state: State) -> list:
+        child = []
+        for i in range(len(state.pipes)):
+            for j in range(len(state.pipes)):
+                if i == j:
+                    continue
+                if not state.pipes[j].is_full() and not state.pipes[i].is_empty():
+                    s = State(copy.deepcopy(state.pipes), state, self.back_get_cost_from_change(state, i), (i, j))
+                    s.change_between_two_pipe(i, j)
+                    child.append(State(copy.deepcopy(s.pipes), state, self.back_get_cost_from_change(state, i), (i, j)))
+        return child
+
     def successor_ucs(self, state: State) -> list:
         child = []
         for i in range(len(state.pipes)):
@@ -79,6 +92,16 @@ class Problem:
             return state.g_n + self.path_cost[2]
         elif state.pipes[pipe_src_ind].stack[-1] == 'yellow':
             return state.g_n + self.path_cost[3]
+
+    def back_get_cost_from_change(self, state: State, pipe_src_ind: int) -> int:
+        if state.pipes[pipe_src_ind].stack[-1] == 'red':
+            return state.g_n - self.path_cost[0]
+        elif state.pipes[pipe_src_ind].stack[-1] == 'blue':
+            return state.g_n - self.path_cost[1]
+        elif state.pipes[pipe_src_ind].stack[-1] == 'green':
+            return state.g_n - self.path_cost[2]
+        elif state.pipes[pipe_src_ind].stack[-1] == 'yellow':
+            return state.g_n - self.path_cost[3]
 
     @staticmethod
     def get_cost_from_change_ucs(state: State, pipe_src_ind: int, pipe_dest_ind: int) -> int:
